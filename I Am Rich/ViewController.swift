d3e79301b5625b0a746034237a19185b4273025f
Artem <Tkachuk>
2020-07-15 21:42:43 -0700

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Artem Tkachuk on 7/15/20.
+//  Copyright © 2020 Artem Tkachuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
